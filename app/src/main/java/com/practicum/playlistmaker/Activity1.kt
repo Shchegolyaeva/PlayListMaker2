ca657c012c3121e2e1c594040963bfc72b5e4128
shchegolyaevaOlesya <shchegolyaeva32@yandex.ru>
2022-12-07 17:26:13 +0300

    For first rewiev

--- app/src/main/java/com/practicum/playlistmaker/Activity1.kt
+++ app/src/main/java/com/practicum/playlistmaker/Activity1.kt
@@ -2,10 +2,73 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.internal.ViewUtils.hideKeyboard
+
 
 class Activity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
+
+        val inputEditText = findViewById<EditText>(R.id.search_content)
+        val clearButton = findViewById<Button>(R.id.exit)
+
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            hideKeyboard(currentFocus ?: View(this))
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    companion object {
+        const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val inputEditText = findViewById<EditText>(R.id.search_content)
+        outState.putString(PRODUCT_AMOUNT, inputEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val inputEditText = findViewById<EditText>(R.id.search_content)
+        val text = savedInstanceState.getString(PRODUCT_AMOUNT, "")
+        if (!text.isNullOrEmpty()) {
+            inputEditText.setText(text)
+
+        }
     }
 }
